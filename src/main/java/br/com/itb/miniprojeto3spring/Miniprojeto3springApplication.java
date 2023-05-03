8de114600005d90979d10a4f8b711613a68915d3
Lavinia2006 <rm82847@estudante.fieb.edu.br>
2023-05-03 09:13:14 -0300

    first commit

---
+++ src/main/java/br/com/itb/miniprojeto3spring/Miniprojeto3springApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itb/miniprojeto3spring/Miniprojeto3springApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
