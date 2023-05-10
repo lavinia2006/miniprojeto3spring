515f7c66479667fe315e82db6dbfc3d5203e5c58
Lavinia2006 <rm82847@estudante.fieb.edu.br>
2023-05-10 12:21:02 -0300

    UPDATE

---
+++ src/main/java/br/com/itb/miniprojeto3spring/model/Produto.java
@@ -0,0 +1,28 @@
+package br.com.itb.miniprojeto3spring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Produto")
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;			 
+	private String nome;  
+	private String descricao;
+	private String codigoBarras;
+	private byte foto;	 
+	private double preco;	
+	private String categoria;
+	private String destaque;
+	private String statusProd;
+	
+	
+	
+}
